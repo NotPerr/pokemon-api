d91917d211b143ad1b0469ff0e371449391be86f
NotPerr <vovo7292@gmail.com>
2023-02-28 00:34:34 +0800

    v 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  // ---------------- set search value ---------------- 
+  const [search,setSearch] = useState('')
+  const [hasSearch,setHasSearch] = useState(null)
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+    console.log('search:',search)
+  }
+
+  // ---------------- set pokemon ---------------- 
+  const [pokemon,setPokemon] = useState({});
+
+  // ---------------- fetch api data ---------------- 
+  
+  
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
+      .then(
+        (response) => {
+          console.log(response)
+          setPokemon(response.data)
+          setHasSearch(true)
+        }
+      )
+  }
+
+  const Display = () => {
+    return (
+      <div>
+        <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+        <h3>{pokemon.name}</h3>
+        <p>type: {pokemon.types[0].type.name}</p>
+        <p>hp: {pokemon.stats[0].base_stat}</p>
+        <p>attack: {pokemon.stats[1].base_stat}</p>
+        <p>defense: {pokemon.stats[2].base_stat}</p>
+        <p>speed: {pokemon.stats[5].base_stat}</p>
+      </div>
+    )
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div>
+        <br />
+        search pokemon: <input value={search} onChange={handleSearchChange}/>
+      </div>
+      <button onClick={searchPokemon}>search</button>
+      <br/>
+      {hasSearch ? 
+        <Display />
+        : <>search a pokemon</>
+        }
+      
     </div>
   );
 }
